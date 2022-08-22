d538d110595db83533fa2c0861cf6935b58fb5b7
unknown <nneodani@gmail.com>
2022-08-22 23:45:03 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Counter from './components/Counter';
 
 function App() {
-
   const [showCounter, setShowCounter] = useState(true)
-
   return (
-    <div className="App">
+    <div className="App"> 
       <h1>Hello world</h1>
-      {showCounter && <Counter />}
+      <button onClick={()=> setShowCounter(showCounter => !showCounter)} >Show counter</button> 
+      {showCounter && <Counter onChangeState={()=>showCounter} ></Counter>}     
     </div>
   );
 }
-
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from 'react'
 
-export default function Counter() {
-
-  const [count, setCount] = useState(0);
+export default function Counter(onChangeState)  {
+  const [count, setCount] = useState();
   const [dynamicClass, setDynamicClass] = useState('text');
 
+ 
+
   // First iteration
+  const handleIncrease =(num)=>{
+      setCount(count => count + num);
+  }
 
   // Second and Fourth iteration
+  useEffect(() => {
+    setCount(count => count =      
+      Math.floor(Math.random() * 10)      
+    )
+  },[]);
 
   // Third iteration
 
+  useEffect(() => {
+      if(count < 0){
+        setDynamicClass(dynamicClass=>dynamicClass = "red");      
+      }else if (count >-1 && count < 6 ){
+        setDynamicClass(dynamicClass=>dynamicClass = "orange");      
+      }else {
+        setDynamicClass(dynamicClass=>dynamicClass = "green");      
+      }
+  },[count])
+  useEffect(() => {
+       setCount(count => count =0)
+       setDynamicClass(dynamicClass=>dynamicClass = "text");
+       console.log('All clean here. Count:', count, 'Class:', dynamicClass);
+  },[onChangeState])
+
+ 
   return (
     <div>
-      <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <p>Counter: <span style={{fontWeight:"bold"}} className={dynamicClass}>{count}</span></p>
+      <button onClick={()=>handleIncrease(1)}> Increase</button>
+      <button onClick={()=>handleIncrease(-1)}>- Decrease</button>    
     </div>
   )
 }
